7ae712503cc779ea7dd3e2dc571a085b69ad7cb0
MoringStr <MoringStr010@gmail.com>
2020-04-19 10:53:56 +0800

    初始化

---
+++ TEST-S-APP/TEST-S-APP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TEST-S-APP
+//
+//  Created by MoringStar on 2020/4/19.
+//  Copyright © 2020 MoringStar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
